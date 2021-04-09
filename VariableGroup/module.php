ea7b68268ac7ae2af6855a2aebd3a7c1f6ba8246
Andreas Seemueller <andreas.seemueller@gmail.com>
2021-04-09 23:49:22 +0200

    Changed logging approach

--- VariableGroup/module.php
+++ VariableGroup/module.php
@@ -140,10 +140,12 @@ class VariableGroup extends IPSModule {
 
 	}
 	
+	// Version 1.0
 	protected function LogMessage($message, $severity = 'INFO') {
 		
 		$logMappings = Array();
-		$logMappings['DEBUG'] 	= 10206;
+		// $logMappings['DEBUG'] 	= 10206; Deactivated the normal debug, because it is not active
+		$logMappings['DEBUG'] 	= 10201;
 		$logMappings['INFO']	= 10201;
 		$logMappings['NOTIFY']	= 10203;
 		$logMappings['WARN'] 	= 10204;
