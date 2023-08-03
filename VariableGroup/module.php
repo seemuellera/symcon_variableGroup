54210f2e193e253c425a1edb77f54a8189fc92a6
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-08-03 21:57:42 +0200

    VM_UPDATE

--- VariableGroup/module.php
+++ VariableGroup/module.php
@@ -60,7 +60,7 @@ class VariableGroup extends IPSModule {
 		$messagesList = $this->GetMessageList();
 		foreach ($messagesList as $currentMessage) {
 
-			$this->UnregisterMessage($currentMessage, VM_CHANGE);
+			$this->UnregisterMessage($currentMessage, VM_UPDATE);
 		}
 		
 		$sourceVariables = $this->GetSourceVariables();
